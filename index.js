5d2470e577c4712edec7d15f9493e2fd90cd52cc
uedayou <yooueda@gmail.com>
2020-09-14 19:12:25 +0900

    歴史地名データのWeb APIでの検索に対応

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const hpEnrichment = require("./lib/hp-main");
 const reverseGeocoder = require("./lib/reverse-geocoder");
 
 module.exports = function(src, type) {
-	db_name = type=="old" ? "hpdb" : "db";
-  if (src.lat && src.lng || src["@type"] == "座標型") {
+  if (src["住所"] && src["住所"]["種別"] == "歴史地名データ")
+    type = "old";
+  db_name = type=="old" ? "hpdb" : "db";
+  if (src.lat && src.lng || (
+    src["@type"] == "座標型" ||
+    src["地理座標"] && src["地理座標"]["@type"] == "座標型")) {
     return reverseGeocoder(src, db_name);
   } else if (db_name == "hpdb") {
     return hpEnrichment(src);
